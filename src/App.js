c7c1f58c84321d4760d8eb74128a89f547208fff
derells <adenijiadenrele225@gmail.com>
2022-10-31 14:31:16 +0100

    boobs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PageWrapper from "./components/PageWrapper";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // Pages
 import Home from "./components/pages/Home";
@@ -9,25 +10,77 @@ import Services from "./components/Common/Services";
 import Contact from "./components/pages/Contact";
 import Portfolio from "./components/Common/Portfolio";
 import Team from "./components/Common/Team";
+import Login from "./components/pages/Login";
+import AdminWrapper from "./components/AdminWrapper";
 
 class App extends Component {
   render() {
     return (
       <Router>
-          <Routes>
-            <Route exact={true} path="/" element={<Home />} />
+        <Routes>
+          <Route path="/admin" element={<Login />}>
+            render =
+            {(props) => (
+              <AdminWrapper>
+                <Login />
+              </AdminWrapper>
+            )}
+          </Route>
 
-            <Route path="/about" element={<About />} />
+          <Route exact={true} path="/" element={<Home />}>
+            render =
+            {(props) => (
+              <PageWrapper>
+                <Home {...props} />
+              </PageWrapper>
+            )}
+          </Route>
 
-            <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />}>
+            render ={" "}
+            {(props) => (
+              <PageWrapper>
+                <About {...props} />
+              </PageWrapper>
+            )}
+          </Route>
 
-            <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/services" element={<Services />}>
+            render ={" "}
+            {(props) => (
+              <PageWrapper>
+                <Services {...props} />
+              </PageWrapper>
+            )}
+          </Route>
 
-            <Route path="/team" element={<Team />} />
+          <Route path="/portfolio" element={<Portfolio />}>
+            render ={" "}
+            {(props) => (
+              <PageWrapper>
+                <Portfolio {...props} />
+              </PageWrapper>
+            )}
+          </Route>
 
-            <Route path="/Contact" element={<Contact />} />
-          </Routes>
-        </PageWrapper>
+          <Route path="/team" element={<Team />}>
+            render ={" "}
+            {(props) => (
+              <PageWrapper>
+                <Team {...props} />
+              </PageWrapper>
+            )}
+          </Route>
+
+          <Route path="/contact" element={<Contact />}>
+            render ={" "}
+            {(props) => (
+              <PageWrapper>
+                <Contact {...props} />
+              </PageWrapper>
+            )}
+          </Route>
+        </Routes>
       </Router>
     );
   }
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,8 +1,91 @@
 import React, { Component } from "react";
+import Fields from "../Common/Fields";
+import { withFormik } from "formik";
+import { Connect } from "react-redux";
+import * as authActions from "../../store/actions/authActions";
+import * as Yup from "yup";
 
+const fields = [
+  {
+    name: "email",
+    elementName: "input",
+    type: "email",
+    placeholder: "Your email",
+  },
+  {
+    name: "password",
+    elementName: "input",
+    type: "password",
+    placeholder: "Your Password",
+  },
+];
 class Login extends Component {
   render() {
-    return <h1>Login</h1>;
+    return (
+      <div className="login-page">
+        <div className="container">
+          <div className="login-form">
+            <div className="row">
+              <h1>Login</h1>
+            </div>
+            <div className="row">
+              <form onSubmit={this.props.handleSubmit}>
+                {fields.map((f, i) => {
+                  return (
+                    <div className="col-md-12">
+                      <Fields
+                        key={i}
+                        {...f}
+                        value={this.props.values[f.name]}
+                        name={f.name}
+                        onChange={this.props.handleChange}
+                        onBlur={this.props.handleBlur}
+                        touched={this.props.touched[f.name]}
+                        errors={this.props.errors[f.name]}
+                      />
+                    </div>
+                  );
+                })}
+                <div className="col-md-12">
+                  <button className="btn btn-primary">Login</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
-export default Login;
+
+const mapStateToProps = (state) => {
+  return {
+    auth: state.auth,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    login: (email, pass) => {
+      dispatch(authActions.login(email, pass));
+    },
+  };
+};
+
+export default connect(
+  withFormik({
+    mapPropsToValues: () => ({
+      email: "",
+      password: "",
+    }),
+    validationSchema: Yup.object().shape({
+      email: Yup.string()
+        .email("Email is invalid")
+        .required("You need to use a real email address"),
+      password: Yup.string().required("You need to enter your password"),
+    }),
+    handleSubmit: (values, { setSubmitting }) => {
+      console.log("login attempt", values);
+    },
+  })
+)(Login);
